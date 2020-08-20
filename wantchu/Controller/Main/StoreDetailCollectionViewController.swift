f6bb486ce3d642f91a446892cf8e343ac6811324
Mingu Kang <minqu.kang@gmail.com>
2020-08-20 21:22:54 +0900

    ORANGE

--- wantchu/Controller/Main/StoreDetailCollectionViewController.swift
+++ wantchu/Controller/Main/StoreDetailCollectionViewController.swift
@@ -20,7 +20,7 @@ class StoreDetailCollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .black
+        view.backgroundColor = .orange
         
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
